4d36995e88cfbf8fedd37cdf8dfd75f5c05b841a
BRUNOOLIVEIRA13 <bruno.henrique0liveir4@gmail.com>
2019-12-28 16:21:53 -0300

    Course 2 - R Programming - Coursera

---
+++ 2_R_Programming/W2 - Functions/Functions.R
@@ -0,0 +1,58 @@
+soma2 <- function(x, y){
+  x + y
+}
+
+above10 <- function(x, n = 10){
+  use <- x > n
+  x[use]
+  
+}
+##
+above <- function(x, n) {
+  use <- x > n
+  x[use]
+}
+
+##
+columnmean <- function(y, removeNA = TRUE){
+  nc <- ncol(y)
+  means <- numeric(nc)
+  for(i in 1:nc){
+    means[i] <- mean(y[, i], na.rm = TRUE)
+  }
+  means
+}
+##
+make.power <- function(n){
+  pow <- function(x){
+    x^n
+  }
+  pow
+}
+
+##
+y <- 10
+
+f <- function(X){
+  y <- 2
+  y^2 + g(x)
+}
+
+g <- function(x){
+  x*y
+}
+
+## function opt
+
+make.NegLogLik <- function(data, fixed=c(FALSE, FALSE)) {
+  params <- fixed
+  function(p){
+    params[!fixed] <- p
+    mu <- params[1]
+    sigma <- params[2]
+    a <- -0.5*length(data)*log(2*pi*sigma^2)
+    b <- -0.5*sum((data-mu)^2) / (sigma^2)
+    -(a + b)
+  }
+  
+}
